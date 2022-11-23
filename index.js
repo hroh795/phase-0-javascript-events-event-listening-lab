c3b67aa3052bed5df5fe2c0445523309bba8ef97
Ha Young Roh <hyroh795@gmail.com>
2022-11-22 20:48:16 -0500

    commit tree e9c56931f3b1a2e1d039ab10089b41fcef78a256

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const input = document.getElementById('button');
+
+
 function addingEventListener() {
+
+
+input.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
 }
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+
+input.addingEventListener('click', clickAlert);
